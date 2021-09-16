6af14c40ba54bd91867e6ee648473e5bbacc6e60
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-09-16 15:02:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,14 @@
 # Copyright permission under MIT License
 # All rights reserved by FayasNoushad
 # License -> https://github.com/FayasNoushad/Country-Info-API/blob/main/LICENSE
+
+from flask import Flask, redirect, request, jsonify, json
+from countryinfo import CountryInfo
+
+
+app = Flask(__name__)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host="0.0.0.0", port=5000, use_reloader=True, threaded=True)
