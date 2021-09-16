fb0ef470ff663d393b725ee70aa4c2136708f1b9
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-09-16 15:04:54 +0530

    Added main route of API

--- main.py
+++ main.py
@@ -11,6 +11,21 @@ from countryinfo import CountryInfo
 app = Flask(__name__)
 
 
+@app.route("/")
+def main():
+    if request.args.get('query'):
+        query = request.args.get('query')
+    else:
+        return redirect("https://github.com/FayasNoushad/Country-Info-API")
+    country = CountryInfo(query)
+    info = country.info()
+    if info is not None:
+        info["google"] = "https://www.google.com/search?q=" + info["name"].replace(" ", "+")
+        return jsonify(info)
+    else:
+        return "Something wrong"
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host="0.0.0.0", port=5000, use_reloader=True, threaded=True)
